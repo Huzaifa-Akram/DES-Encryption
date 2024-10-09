6fa587d79ecaf26685f93478a8f8e0aeccb56a96
Huzaifa Adil Akram <137613616+Huzaifa-Akram@users.noreply.github.com>
2024-10-09 23:09:18 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+        
+        
         const PC1 = [57, 49, 41, 33, 25, 17, 9, 1, 58, 50, 42, 34, 26, 18, 10, 2, 59, 51, 43, 35, 27, 19, 11, 3, 60, 52, 44, 36, 63, 55, 47, 39, 31, 23, 15, 7, 62, 54, 46, 38, 30, 22, 14, 6, 61, 53, 45, 37, 29, 21, 13, 5, 28, 20, 12, 4];
         const PC2 = [14, 17, 11, 24, 1, 5, 3, 28, 15, 6, 21, 10, 23, 19, 12, 4, 26, 8, 16, 7, 27, 20, 13, 2, 41, 52, 31, 37, 47, 55, 30, 40, 51, 45, 33, 48, 44, 49, 39, 56, 34, 53, 46, 42, 50, 36, 29, 32];
         const IP = [58, 50, 42, 34, 26, 18, 10, 2, 60, 52, 44, 36, 28, 20, 12, 4, 62, 54, 46, 38, 30, 22, 14, 6, 64, 56, 48, 40, 32, 24, 16, 8, 57, 49, 41, 33, 25, 17, 9, 1, 59, 51, 43, 35, 27, 19, 11, 3, 61, 53, 45, 37, 29, 21, 13, 5, 63, 55, 47, 39, 31, 23, 15, 7];
@@ -187,3 +189,21 @@
             const finalCiphertext = permute(preOutput, FP);
             addResult("Final Ciphertext", formatBinary(finalCiphertext, 8));
         }
+
+
+
+        const clickSound = document.getElementById('click-sound');
+        const typingSound = document.getElementById('typing-sound');
+
+        document.querySelectorAll('button').forEach(button => {
+            button.addEventListener('click', function() {
+                clickSound.play();
+            });
+        });
+
+
+        document.querySelectorAll('input[type="text"]').forEach(input => {
+            input.addEventListener('keydown', function() {
+                typingSound.play();
+            });
+        });
